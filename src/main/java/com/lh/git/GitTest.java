966e6fcc026e860ee1ac408f13ff2c84bba4b7c2
lh <1309625119@qq.com>
2021-11-19 12:58:17 +0800

    hot-fix commit3

--- src/main/java/com/lh/git/GitTest.java
+++ src/main/java/com/lh/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hot-fix test");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
     }
 }
