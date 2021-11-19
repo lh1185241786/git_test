9d28d51da4fad0008d5e965a05d37bc9b0cd050c
lh <1309625119@qq.com>
2021-11-19 13:00:20 +0800

    hot-fix commit5

--- src/main/java/com/lh/git/GitTest.java
+++ src/main/java/com/lh/git/GitTest.java
@@ -4,13 +4,6 @@ import java.util.Arrays;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("1231321321t!");
-        System.out.println(Arrays.asList(1,2,3,4,5));
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hot-fix test");
-        System.out.println("11111111111111111");
-        System.out.println("11111111111111111");
+        int i =1;
     }
 }
